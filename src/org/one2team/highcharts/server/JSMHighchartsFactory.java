6cbbc23bd66de7fc264eca11f16917704cfaabc2
vbox <vbox@vbox-ubuntu.(none)>
2011-12-30 19:35:06 +0100

    svg  export

--- src/org/one2team/highcharts/server/JSMHighchartsFactory.java
+++ src/org/one2team/highcharts/server/JSMHighchartsFactory.java
@@ -2,7 +2,6 @@ package org.one2team.highcharts.server;
 
 import org.one2team.highcharts.shared.ChartOptions;
 import org.one2team.highcharts.shared.HighchartsFactory;
-import org.one2team.highcharts.shared.Point;
 import org.one2team.highcharts.shared.Series;
 
 public class JSMHighchartsFactory implements HighchartsFactory {
